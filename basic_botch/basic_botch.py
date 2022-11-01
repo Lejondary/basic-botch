966278c136997b896dd58522f8d21765d80ac21e
Lejondary <jonathanphari@gmail.com>
2022-10-31 17:06:02 -0700

    Added members intent, backend log off notification, and owner_id print statement

--- basic_botch/basic_botch.py
+++ basic_botch/basic_botch.py
@@ -7,13 +7,19 @@ import os
 import constant
 
 intents = discord.Intents.default()
-intents.message_content = True
+intents.message_content = True # Allows bot to check message content
+intents.members = True # Allows bot to track member/user updates
 
 class BasicBotch(commands.Bot):
 
     # Setups the bot before websocket login 
     async def setup_hook(self):
-        print(f'Logged in as {self.user} on Discord.\n', flush = True)
+
+        # Prints bot username when successfully connected on Discord
+        print(f'Logged in as {self.user} on Discord!\n', flush = True)
+
+        # Prints its creator's owner id
+        print(f'Herro, I was created by {self.owner_id}\n')
 
         # Load extensions in Cogs folder
         print(f'Loading Cogs...\n')
@@ -23,6 +29,8 @@ class BasicBotch(commands.Bot):
 
 botch = BasicBotch(command_prefix=constant.PREFIX, description="I'm a basic botch.", intents=intents)
 botch.run(constant.TOKEN)
+# Notifies when bot has logged off and disconnected from Discord 
+print(f'Logged out of {botch.user} on Discord!\n')
 
 # Asynchronous setup AFTER bot login
 #async def main():
